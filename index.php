8b3be7bbc1fc377f89ba4070309c596bda67ef24
rafaelharthopf <harthopfpereirarafael@gmail.com>
2025-02-12 17:53:43 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagamento Inicial</title>
+    <link rel="stylesheet" href="lib/style.css">
+</head>
+<body>
+    <div class="product">
+        <div class="product_title"> Pend Drive</div>
+        <div class="product_image"><img src="img/pendrive.jpg" alt="pendrive"></div>
+        <div class="product_desc">Pen Drive de 16GB</div>
+        <div class="product_btn"><a href="https://mpago.la/1wC9XZm">Pagar</a></div>
+    </div>
+</body>
+</html>
